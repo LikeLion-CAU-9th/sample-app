1859b227fcf06cc010e6c86466798d06879e8c42
seungahkim <safg0421@cau.ac.kr>
2021-11-10 18:09:26 +0900

    feat: add locust files

--- django-tutorial/mysite/urls.py
+++ django-tutorial/mysite/urls.py
@@ -1,7 +1,9 @@
 from django.urls import include, path
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
 ]
